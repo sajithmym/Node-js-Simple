c96e92bb2edaa815637d45a403f3eb264aec98af
Sajithmym <Sajithmym@gmail.com>
2023-01-21 11:57:40 +0530

    First Node js Program

---
+++ First.js
@@ -0,0 +1,6 @@
+console.log(`\n\tFile Path is ${__filename}`);
+console.log(`\tFile Directry  is ${__dirname}`);
+
+setInterval(() => {
+    console.log("\t\t\tWelcome to Sajithmym Node js....");
+},1000)
