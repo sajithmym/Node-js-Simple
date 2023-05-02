7e88b41f0128dd8909edb6fde0709a15197f4c0a
Sajithmym <Sajithmym@gmail.com>
2023-05-02 11:46:07 +0530

    dj

--- Now 3/Route/get.js
+++ Now 3/Route/get.js
@@ -1,10 +1,10 @@
 const exp = require("express")
+const All = require("./post")
 
 const Dude_route = exp.Router()
 
 Dude_route.get("/",(req,res,n) => {
-    console.log("I am Root");
-    res.render("Home",{title : "Home page"})
+    res.render("Home",{title : "Home page",mym : All.all})
 })
 
 module.exports.r = Dude_route
--- Now 3/Route/post.js
+++ Now 3/Route/post.js
@@ -2,19 +2,20 @@ const exp = require("express")
 
 const Dude_route = exp.Router()
 
+const Users = []
+
 Dude_route.get("/a",(req,res,n) => {
-    console.log("I am a");
     res.render("add",{ title : "Adding Page"})
 })
 
 Dude_route.post("/goto",(r,res,n) => {
-    console.log("I am Post");
+    Users.push({name : r.body.sj})
     res.redirect('/')
 })
 
 Dude_route.use('/',(req,res,n) => {
-    console.log("page not found...");
     res.render('ms')
 })
 
 module.exports.r = Dude_route
+module.exports.all = Users
