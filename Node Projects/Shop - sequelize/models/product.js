9386359d484dd054dad9f121c883784ea6eb9952
Sajithmym <Sajithmym@gmail.com>
2023-05-20 12:00:50 +0530

    pro

--- Node Projects/Shop - sequelize/models/product.js
+++ Node Projects/Shop - sequelize/models/product.js
@@ -1,30 +1,5 @@
-const db = require('../util/database');
+const Sequelize = require('sequelize');
 
-const Cart = require('./cart');
+const dbm = require('../util/database');
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
-
-  save() {
-    return db.execute(
-      'INSERT INTO product (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  static deleteById(id) {}
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM product');
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM product WHERE product.id = ?', [id]);
-  }
-};
+const prodect = dbm.define()
