d192564f48f1a0096b1dc9eeb80e69b950863848
Sajithmym <Sajithmym@gmail.com>
2023-05-27 21:08:57 +0530

    work

--- Node Projects/Shop - sequelize/controllers/shop.js
+++ Node Projects/Shop - sequelize/controllers/shop.js
@@ -27,15 +27,16 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
-      res.render('shop/index', {
-        prods: rows,
-        pageTitle: 'Shop',
-        path: '/'
-      });
-    })
-    .catch(err => console.log(err));
+  Product.findAll()
+  .then((result) => {
+    res.render('shop/index', {
+      prods: result,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
 };
 
 exports.getCart = (req, res, next) => {
