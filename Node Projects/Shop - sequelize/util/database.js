40ab663b5f2ba5ae93878a16d811c04f0b97490c
Sajithmym <Sajithmym@gmail.com>
2023-05-27 20:08:43 +0530

    now

--- Node Projects/Shop - sequelize/util/database.js
+++ Node Projects/Shop - sequelize/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('pro_3', 'root', '', {
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
   dialect: 'mysql',
   host: 'localhost'
 });
