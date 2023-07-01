34b86f9a73063e89afe778c577c1173a55ff868a
Sajithmym <sajmymvcool@gmail.com>
2023-07-01 12:03:40 +0530

    sa

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var http = require("http")
+var http = require("http");
 http.createServer(function (request,response)  {
-    response.writeHead(200,{ 'Content-Type' : 'text/plain'})
-    response.end("\n \t\tWelcome to Sajithmym\n ")
+    response.writeHead(200,{ 'Content-Type' : 'text/plain'});
+    response.end("\n \t\tWelcome to Sajithmym\n ");
 }).listen(8011)
 
-console.log(`Server Start in http://127.0.0.1:8011`)
+console.log(`Server Start in http://127.0.0.1:8011`);
